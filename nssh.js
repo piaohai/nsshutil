4017880038620102ff1ef2fa36c2bace72def1cd
FIRST_NAME LAST_NAME <yaopiaohai@corp.netease.com>
2013-05-20 10:41:53 +0800

    misc:add config

--- nssh.js
+++ nssh.js
@@ -1,14 +1,14 @@
 var Connection = require('ssh2');
-var rl = require('readline'),
-    sys = require('sys');
+var rl = require('readline');
+var sys = require('sys');
 var fs = require('fs');
 
-var Config = require('./lib/preconfig');
+var PreUtil = require('./lib/preconfig');
 
 process.stdin.setEncoding('utf8');
 
-var publicKey = fs.readFileSync(Config.config.publicKey) || '';
-var privateKey = fs.readFileSync(Config.config.privateKey) || '';
+var publicKey = fs.readFileSync(PreUtil.config.publicKey) || '';
+var privateKey = fs.readFileSync(PreUtil.config.privateKey) || '';
 
 var firstMap = {};
 
@@ -17,93 +17,131 @@ lines.setPrompt('>:');
 
 var clients = {};
 
-var connect = function(host,config) {
-    var c = new Connection();
-    c.on('connect', function() {
-      //console.log(host + ':: connect');
-    });
-    c.on('ready', function() {
-      console.log('------------' + host + ' :: ready ----------');
-      firstMap[host] = true;
-      c.shell(function(err, stream) {
-        if (err) throw err;
-        stream.on('data', function(data, extended) {
-          var tdata = data+'';
-          //console.log(data);
-          //console.log(tdata);
-          if (!tdata) return;
+var switchConnect = function(host,config) {
+  var c = new Connection();
+  c.on('ready', function() {
+    firstMap[host] = false;
+    c.shell(function(err, stream) {
+      if (err) throw err;
+      var lastcmd = 'sudo -i -u ' + config.destname;
+      stream.write(lastcmd);
+      stream.write('\n');
+      stream.on('data', function(data, extended) {
+        if (!data) return;
+        var tdata = data+'';
+        if (tdata.indexOf(lastcmd)!=-1 || tdata[tdata.length-2]==='$'){
           lines.prompt();
-          console.log('------------' + host + ' ----------');
-          //if (tdata!==last){
-            if (!firstMap[host])
-             {
-              if (!!tdata) {
-                console.log(tdata);
-              }
-            }
-            //last = tdata;
-          //} 
-          lines.prompt();
-          firstMap[host] = false;
-        });
-        stream.on('end', function() {
-        });
-        stream.on('close', function() {
-          lines.prompt();
-        });
-        clients[host] = stream;
-        stream.on('exit', function(code, signal) {
-        });
+          if (tdata[tdata.length-2]==='$') {
+            if (!!firstMap[host]) {
+              c.end();
+            };
+            firstMap[host] = true;
+            lastcmd = " echo '" +  publicKey + "' >> /home/"+config.destname+"/.ssh/authorized_keys";
+            stream.write(lastcmd);
+            stream.write('\n');
+          }
+        }
       });
-      lines.prompt();
+      stream.on('end', function() {
+      });
+      stream.on('close', function() {
+        lines.prompt();
+      });
+      stream.on('exit', function(code, signal) {
+      });
     });
-    c.on('error', function(err) {
-      console.log(host + ' Connection :: error :: ' + err);
+    lines.prompt();
+  });
+  c.on('error', function(err) {
+    console.log(host + ' Connection :: error :: ' + err);
+  });
+  c.on('close', function(had_error) {
+    directConnect(host,config);
+  });
+  c.connect({host: host,
+    port: config.port,
+    username: config.username,
+    passphrase:config.passphrase || '' ,
+    privateKey: privateKey
+  });
+};
+
+var directConnect = function(host,config) {
+  var c = new Connection();
+  c.on('ready', function() {
+    console.log('------------' + host + ' :: ready ----------');
+    c.shell(function(err, stream) {
+      if (err) throw err;
+      stream.on('data', function(data, extended) {
+        if (!data) return;
+        var tdata = data+'';
+        lines.prompt();
+        if (tdata[tdata.length-2]==='$') {
+          console.log('------------' + host + ' ----------');
+          console.log(tdata);
+          lines.prompt();
+        }
+      });
+      stream.on('end', function() {
+      });
+      stream.on('close', function() {
+        lines.prompt();
+      });
+      clients[host] = stream;
+      stream.on('exit', function(code, signal) {
+      });
     });
-    c.on('end', function() {
-      console.log('Connection :: end');
-    });
-    c.on('close', function(had_error) {
-      console.log(host + ' Connection :: close');
-      delete clients[c];
-    });
-    c.connect({host: host,
-      port: config.port,
-      username: config.username,
-      passphrase:config.passphrase || '' ,
-      debug: function (msg) {
+    lines.prompt();
+  });
+  c.on('error', function(err) {
+    console.log(host + ' Connection :: error :: ' + err);
+  });
+  c.on('end', function() {
+  });
+  c.on('close', function(had_error) {
+    console.log(host + ' Connection :: close');
+    delete clients[c];
+  });
+  c.connect({host: host,
+    port: config.port,
+    username: config.destname,
+    passphrase:config.passphrase || '' ,
+    debug: function (msg) {
         //console.log('Connection: ' + msg);
       },
       privateKey: privateKey
     });
 };
- 
-Config.do(connect);
 
+if (PreUtil.config.username!=PreUtil.config.destname) {
+  PreUtil.do(switchConnect);
+} else {
+  PreUtil.do(directConnect);
+}
 
 lines.on('line', function(chunk) {
-    if (chunk==='exit'){
-      process.exit(0);
-    }
-    if (!!chunk) {
-      exec(chunk);
-    } else {
-      lines.prompt();
-    }
+  if (chunk==='exit'){
+    process.exit(0);
+  }
+  if (!!chunk) {
+    exec(chunk);
+  } else {
+    lines.prompt();
+  }
 });
 
 lines.on('close', function () {
-    sys.puts('process end.');
-    process.exit(0);
+  sys.puts('process end.');
+  process.exit(0);
 });
-  var last = "";
+
+var last = "";
 
 var exec = function(chunk) {
   for (var host in clients) {
     var c = clients[host];
     (function(c,host){
-       c.write(chunk + '\n');
-    })(c,host);
-  }
+     c.write(chunk + '\n');
+   })(c,host);
+ }
 };
- 
--- server.js
+++
@@ -1,9 +0,0 @@
-var net = require('net');
-var server = net.createServer(function(socket) {
-	socket.setTimeout(4000,function(){
-		//console.log('timeout');
-		socket.end();
-	})
-});
-
-server.listen(8124);
